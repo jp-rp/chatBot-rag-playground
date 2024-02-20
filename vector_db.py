b52a2c43fbf9ea7d51b8dccf2eee30504063cab4
vt22223 <vt22223@bristol.ac.uk>
2024-02-20 22:12:21 +0000

    Removed unused imports

--- vector_db.py
+++ vector_db.py
@@ -1,13 +1,8 @@
-import os
-
-from langchain_community.document_loaders import PyPDFLoader, PyPDFDirectoryLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from dotenv import load_dotenv
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
-load_dotenv()
-API_KEY = os.getenv('OPENAI_API_KEY')
 DATA_PATH = 'data/'
 DB_PATH = 'vector_stores/chromadb'
 
